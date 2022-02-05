d7b52c899891c5f57d1a5124b4a120d4c17f8d9f
hysryt <hysryt@gmail.com>
2022-02-05 19:58:46 +0900

    add: カメラ選択

--- script.js
+++ script.js
@@ -1,11 +1,45 @@
-async function webcam() {
+async function webcam(deviceId) {
 	const display = document.getElementById("display");
+
+	// 現在のカメラ停止
+	if (display.srcObject) {
+		display.srcObject.getTracks().forEach(track => {
+			track.stop();
+		});
+		display.srcObject = null;
+		display.style.height = '';
+	}
+
+	if (!deviceId) return;
+
 	const stream = await navigator.mediaDevices.getUserMedia({
-		video: true,
+		video: {deviceId},
 		audio: false,
 	});
 	display.srcObject = stream;
+	display.style.height = 'auto';
 	display.play();
 }
 
-webcam();
+async function setCameraSelect(onSelect) {
+	const select = document.getElementById("camera-select");
+
+	const deviceList = await navigator.mediaDevices.enumerateDevices();
+	const cameraList = deviceList.filter(device => device.kind === 'videoinput');
+	cameraList.forEach(camera => {
+		const option = document.createElement('option');
+		option.innerText = camera.label || camera.deviceId;
+		option.setAttribute('value', camera.deviceId);
+		select.appendChild(option);
+	});
+
+	select.addEventListener('change', () => {
+		onSelect(select.selectedOptions[0].value);
+	});
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+	setCameraSelect(deviceId => {
+		webcam(deviceId);
+	});
+})
